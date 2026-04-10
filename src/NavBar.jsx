5506d2b35f4765ffce78839ac3182c6596ae7629
its_yashi126 <yashisharma99935@gmail.com>
2026-04-10 14:43:41 +0530

    tailwind_css added

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,20 +1,56 @@
+
 import React from 'react'
 import { Link } from 'react-router-dom'
-
-
 
 export default function NavBar() {
   return (
     <div>
-                          <nav>
-                                      <h1>Logo</h1>
-                                      <ul>
-                                                  <li><Link to="/">Home</Link></li>
-                                                  <li><Link to="/about">About</Link></li>
-                                                  <li><Link to="/services">Services</Link></li>
-                                                  <li><Link to="/contact">Contact</Link></li>
-                                      </ul>""
+      <nav className="bg-slate-500 text-white shadow-md px-6 py-4">
+        <div className="max-w-7xl mx-auto flex items-center justify-between">
+          <h1 className="text-2xl font-bold text-orange-400 cursor-pointer">
+            Logo
+          </h1>
+
+          <ul className="flex items-center gap-8 text-lg font-medium">
+            <li>
+              <Link
+                to="/"
+                className="hover:text-rose-500 transition duration-300"
+              >
+                Home
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/about"
+                className="hover:text-rose-500 transition duration-300"
+              >
+                About
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/services"
+                className="hover:text-rose-500 transition duration-300"
+              >
+                Services
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/contact"
+                className="hover:text-rose-500 transition duration-300"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
   )
 }
+
